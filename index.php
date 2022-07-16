e8299e1dea69b9fb8aaecdad5d1e0a5d9dcf141e
sphillips13 <phi13025@byui.edu>
2022-07-16 12:33:44 -0600

    added index

---
+++ index.php
@@ -0,0 +1,132 @@
+<!-- Affiliate link
+https://exipure.com/affiliates/
+
+Click bank
+https://2de37k8n82x3uv00-70hw13l5z.hop.clickbank.net
+-->
+
+<!DOCTYPE html>
+<html>
+<head>
+  <script async src="https://www.googletagmanager.com/gtag/js?id=G-XV4KWRLY6M"></script>
+  <meta charset="utf-8">
+  <meta name="description" content="Exipure" />
+  <meta name="keywords" content="weight loss, quick weight loss, fast weight loss, rapid weight loss, weight loss help, fat burner, reduce weight, lose weight fast, calorie burner, easy weight loss, metabolism booster" />
+  <title>Exipure</title>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+  <link rel="stylesheet" href="assets/css-dist/styles.min.css">
+  <script src="assets/js-dist/scripts.min.js"></script>
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:wght@400;700&display=swap" rel="stylesheet"> 
+</head>
+
+
+<?php
+?>
+
+<style>
+  .banner-background
+  {
+    background: #016365;
+  }
+  .white-text
+  {
+    color: white;
+  }
+  .black-text
+  {
+    color: black;
+  }
+  .btn {
+    background-color: #016365;
+    border: 2px solid #016365; 
+  }
+  .btn:hover {
+    background-color: white;
+    border: 2px solid #016365; 
+    color: black;
+  }
+  .button {
+    border: none;
+    color: white;
+    padding: 5px 32px;
+    text-align: center;
+    display: inline-block;
+    font-size: 16px;
+    margin: 4px 2px;
+    transition-duration: 0.4s;
+    cursor: pointer;
+    -webkit-appearance: button;
+    -moz-appearance: button;
+    appearance: button;
+    text-decoration: none;
+}
+
+</style>
+
+<body class="content is-size-4 is-size-5-touch is-unselectable">
+
+  <!-- Header -->
+<section class="section banner-background">
+  <div class="container container-limit has-text-white">
+    <div class="columns is-variable is-1">
+      <div class="column is-5 has-text-centered pb-0">
+        <img src="https://exipure.com/assets/images/exp-home.png" style="max-width:500px;width:100%" alt=""/> 
+      </div>
+      <div class="column pb-0">
+      <p class="title is-size-1 is-size-2-tablet is-size-3-mobile is-capitalized mb-4 white-text">The <span class="yellow-text">Tropical Secret</span><br>
+        For Healthy Weight Loss</p>
+      <p class="is-size-3 is-size-4-touch white-text">Support healthy weight loss with Exipure's proprietary blend of 8 <span class="light-blue">exotic</span> nutrients and plants backed by clinical research.</p>
+      </div>
+    </div>
+  </div>
+</section>
+<section class="section pb-0 mb-0 is-paddingless is-hidden">
+  <div class="container container-limit px-2 pt-5 pb-2">
+    <p class="has-text-centered pb-3 mb-0 has-text-grey is-capitalized is-size-4 is-hidden-mobile">Backed by clinical research from:</p>
+  </div>
+</section>
+
+<!-- Order Now -->
+<section style="text-align: center;">  
+  <div>
+      <p>Click the button below to get more information and get a disccounted offer!</p>
+      <button class="button btn" onclick="window.location.href='https://2de37k8n82x3uv00-70hw13l5z.hop.clickbank.net';">Click Here!</button> 
+  </div>
+</section>
+
+<!-- Science -->
+<section class="section is-paddingless">
+  <div class="container container-limit px-2 py-5">
+    <div class="columns mb-5 is-mobile">
+      <div class="column index-column index-column-2 mr-3"></div>
+      <div class="column is-11">
+        <p class="has-text-weight-bold has-text-danger lh1 mb-2">July 2022 - New Scientific Discovery</p>
+        <p class="title is-size-1-desktop is-size-2-tablet is-size-3-mobile is-capitalized lh1 mb-2">Scientists Discover The Real Root-Cause of Your Belly Fat</p>
+        <p class="has-text-grey-dark is-size-3 is-size-4-mobile mb-5">(<strong>Hint</strong> Not Diet or Exercise)</p>
+        <p>Are you struggling to lose weight no matter how much you diet or exercise? Turns out, <strong>it's not your fault</strong>.</p>
+        <p>A 2022 study published in Nature Medicine of 52,000 women and men, the largest of its kind in humans, found just one common factor in every overweight man and woman. <strong>Low brown adipose tissue levels</strong>! They also found common in every skinny person were high brown adipose tissue levels.<sup>1</sup></p>
+        <p>Brown adipose tissue (BAT) also know as brown fat, is not fat at all. Its not a fat storer but a <strong>fat shrinker</strong>. Its brown color comes from its densely packed mitochondria which work 24/7 to <strong>burn calories</strong> from your fat stores and the food you eat into pure, natural energy.</p>
+        <p>Even though BAT makes up a fraction of your weight, it can burn up to <strong>300 times</strong> more calories than any other cell in your body!<sup>2</sup></p>
+        <p class="is-size-6 has-text-grey lh1 is-size-7-mobile"><em>References:<br>
+        1.https://www.nature.com/articles/s41591-020-1126-7<br>
+        2.https://www.ncbi.nlm.nih.gov/pmc/articles/PMC2768171/</em></p>
+      </div>
+    </div>
+  </div>
+</section>
+
+
+<!-- Van Image-->
+<section class="section is-paddingless banner-background white-text">
+    <div class="columns is-vcentered has-text-centered">
+      <div class="column is-narrow has-text-centered"><img src="https://exipure.com/assets/images/generic/shipping.png" width="180" alt=""/></div>
+        <div class="column"><p class="block-10 has-text-white lh1 has-text-weight-bold mb-0 pb-2">Every <u class="yellowunder">6 Bottles</u> Ordered <br class="is-hidden-desktop">Gets <u class="yellowunder">FREE Shipping</u> Too!
+          <p class="is-capitalized">*96% of customers order 6 bottles <br class="is-hidden-desktop">(<strong style="color: #02b6ba;">Our recommended option</strong>)</p></div>	
+      </div>
+</section><section class="section is-paddingless">
+
+
+</body>
+</html>
